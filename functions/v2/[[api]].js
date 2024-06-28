137d08d8cf0e5ffdda97f5036a9de0fd3bbd03b3
yunnysunny <yunnysunny@gmail.com>
2024-06-28 12:56:26 +0800

    remove unused code

--- functions/v2/[[api]].js
+++ functions/v2/[[api]].js
@@ -1,13 +1,3 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run "npm run dev" in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run "npm run deploy" to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export async function onRequest(context) {
   const request = context.request
   const url = new URL(request.url);
