002c9bcfc8c75b2ce0c2f956452adfadbc076259
yunnysunny <yunnysunny@gmail.com>
2024-06-30 14:33:21 +0800

    modify res HEADER_WWW_AUTHENTICATE

--- functions/v2/[[api]].js
+++ functions/v2/[[api]].js
@@ -1,5 +1,5 @@
 import { getRegistryHost } from "../common";
-
+const HEADER_WWW_AUTHENTICATE = 'www-authenticate';
 export async function onRequest(context) {
   const request = context.request
   const url = new URL(request.url);
@@ -20,25 +20,26 @@ export async function onRequest(context) {
   if (registryResponse.headers.get("content-type").indexOf('json') !== -1) {
     content = await registryResponse.clone().json();
   }
-  const wwwAuth = registryResponse.headers.get("www-authenticate");
-  if (wwwAuth && context.env.SELF_AUTH === 'true') {
-    headers.set(
-      "www-authenticate", 
-      wwwAuth.replace(
-        /realm="([^"]+)"/, 
-        `realm="${url.protocol}/${url.host}/v2/auth"`
-      )
-    );
-  }
+
   console.log(
-    request.url, 
-    registryResponse.status, 
+    request.url,
+    registryResponse.status,
     JSON.stringify(Object.fromEntries(new Map(registryResponse.headers))),
     JSON.stringify(content),
   );
   const responseHeaders = new Headers(registryResponse.headers);
   responseHeaders.set("access-control-allow-origin", originalHost);
   responseHeaders.set("access-control-allow-headers", "Authorization");
+  const wwwAuth = responseHeaders.get(HEADER_WWW_AUTHENTICATE);
+  if (wwwAuth && context.env.SELF_AUTH === 'true') {
+    responseHeaders.set(
+      HEADER_WWW_AUTHENTICATE, 
+      wwwAuth.replace(
+        /realm="([^"]+)"/, 
+        `realm="${url.protocol}/${url.host}/v2/auth"`
+      )
+    );
+  }
   return new Response(registryResponse.body, {
     status: registryResponse.status,
     statusText: registryResponse.statusText,
